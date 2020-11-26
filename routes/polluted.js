69ec08e8d24408e5cded73e5a89c27f47d1f7d5a
Sherif koussa <sherif@softwaresecured.com>
2020-11-25 21:43:16 -0500

    Update polluted.js

--- routes/polluted.js
+++ routes/polluted.js
@@ -19,5 +19,17 @@ router.get('/polluted', function(req, res) {
   res.render('polluted', { title: 'Polluted', keys: JSON.stringify(keys, null, 2)});
 });
 
+router.get('/user-files', function(req, res) {
+    var file = req.param('file');
+    if (file.indexOf('..') !== -1) { // BAD
+        // forbid paths outside the /public directory
+        res.status(400).send('Bad request');
+    } else {
+        var absolute = path.resolve('/public/' + file);
+        console.log("Sending file: %s", absolute);
+        res.sendFile(absolute);
+    }
+});
+
 
 module.exports = router;
