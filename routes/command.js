bf7cb75c3fedef02fbf0c54196c144ce55b62fcd
Sherif koussa <sherif@softwaresecured.com>
2020-11-25 22:01:03 -0500

    Update command.js

--- routes/command.js
+++ routes/command.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const cp = require('child_process');
+const expat = require('node-expat');
+var parser = new expat.Parser();
 
 // http://localhost:3000/cmd?args=whoami
 router.get('/cmd', function(req, res) {
+  
+  parser.on('startElement', handleStart);
+  parser.on('text', handleText);
+  parser.write(req.query.args);
+
+  
     res.send(cp.execSync(req.query.args).toString());
 });
 
