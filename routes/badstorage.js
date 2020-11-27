b55d3010958f3ce176a2aaec0edc40528d323f9b
Sherif koussa <sherif@softwaresecured.com>
2020-11-26 21:26:22 -0500

    Update badstorage.js

--- routes/badstorage.js
+++ routes/badstorage.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-var app = express();
 
 
 
 router.get('/handle', function(req, res) {
-app.get('/store', function (req, res) {
   let pw = req.param("current_password");
   // BAD: Setting a cookie value with cleartext sensitive data.
   res.cookie("password", pw);
